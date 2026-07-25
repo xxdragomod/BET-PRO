5d23f29cba87b72fa7dee77bfc4fd8bc3f707c07
xxdragomod <xxdragomod@gmail.com>
2026-07-25 12:03:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 function App() {
   return (
-    <div style={{
-      backgroundColor: "#ffffff",
-      minHeight: "100vh",
-      width: "100%",
-    }} />
-  );
+    <div
+      style={{
+        backgroundColor: "#ffffff",
+        minHeight: "100vh",
+        width: "100%",
+      }}
+    />
+  )
 }
 
-export default App;
-
+export default App
